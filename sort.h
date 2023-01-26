16de7d3f1310773698da7007b3fb36968d1c4269
Zeekdrich <abahisaac16@gmail.com>
2023-01-26 07:44:40 -0800

    adv task

--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
-#ifndef _SORT_H_
-#define _SORT_H_
+#ifndef SORT_H
+#define SORT_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,13 +14,14 @@
 		array[b] = tmp;       \
 	}
 
+
 /**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
+ * struct listint_s - Represents a doubly linked list node.
+ * @n: Integer stored in the node.
+ * @prev: Pointer to the previous element of the list.
+ * @next: Pointer to the next element of the list.
  */
+
 typedef struct listint_s
 {
 	const int n;
@@ -28,34 +29,37 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Default Functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/* Normal Functions */
+void swap_items(int *array, size_t l, size_t r);
 void bubble_sort(int *array, size_t size);
 void swap_nodes(listint_t **list, listint_t *l, listint_t *r);
-void swap_items(int *array, size_t l, size_t r);
-void swap_ints(int *a, int *b);
 void insertion_sort_list(listint_t **list);
 void insertion_sort_list2(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-listint_t *get_tail(listint_t **list);
-
-/* Advanced Functions */
 void shell_sort(int *array, size_t size);
+listint_t *get_tail(listint_t **list);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+void top_down_merge(int *array, int *array_c, size_t l, size_t m, size_t r);
+void split_merge(int *array, int *array_c, size_t size, size_t l, size_t r);
+void selection_sort(int *array, size_t size);
+void quick_sort_range_lomuto(int *array, size_t low, size_t high, size_t size);
+void quick_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
 void max_heapify(int *array, size_t size, size_t base, size_t root);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 int get_max(int *array, int size);
 void radix_counting_sort(int *array, size_t size, int sig, int *buff);
 void radix_sort(int *array, size_t size);
-void top_down_merge(int *array, int *array_c, size_t l, size_t m, size_t r);
-void split_merge(int *array, int *array_c, size_t size, size_t l, size_t r);
-void merge_sort(int *array, size_t size);
-void quick_sort_range_lomuto(int *array, size_t low, size_t high, size_t size);
+void bitonic_merge(int *array, size_t size,
+		   size_t low, size_t n, char ascending);
+void bitonic_sort_split(int *array, size_t size,
+			size_t low, size_t n, char ascending);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_range_hoare(int *array, size_t low, size_t high, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
-#endif /*_SORT_H_*/
+
+#endif
